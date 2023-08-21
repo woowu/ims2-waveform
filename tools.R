07f3bb024a6ff50c288ab274896b64443bf7e4f1
Woody Wu <woody.wu2@landisgyr.com>
2023-08-21 22:35:39 +0800

    tools: fourier tool

--- tools.R
+++ tools.R
@@ -116,3 +116,33 @@ dev_new <- function(n=1, m=1, name = NULL) {
     par(bg='cornsilk', mfrow=c(n, m))
 }
 
+# Plot a fourier series.
+# fourier.series: the function synthesis the series with
+#   arguments t and w.
+# ts: time series
+#
+# E.g.:
+#
+#  acq.freq <- 100                    # data acquisition frequency (Hz)
+#  time     <- 6                      # measuring time interval (seconds)
+#  ts       <- seq(0,time,1/acq.freq) # vector of sampling time-points (s)
+#  f.0      <- 1/time                 # fundamental frequency (Hz)
+#  
+#  dc.component       <- 0
+#  component.freqs    <- c(3,10)      # frequency of signal components (Hz)
+#  component.delay    <- c(0,0)       # delay of signal components (radians)
+#  component.strength <- c(.5,.25)    # strength of signal components
+#  
+#  f <- function(t,w) {
+#    dc.component +
+#    sum( component.strength * sin(component.freqs*w*t + component.delay))
+#  }
+#  
+#  plot.fourier(f,f.0,ts)
+#
+plot.fourier <- function(fourier.series, f.0, ts) {
+    w <- 2 * pi * f.0
+    trajectory <- sapply(ts, function(t) fourier.series(t, w))
+    plot(ts, trajectory, type='l', xlab='time', ylab='f(t)')
+    abline(h=0, lty=3)
+}
