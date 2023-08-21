5f2cd018d7ff39a2986046d9697e8bf6ce8f8d0b
Woody Wu <woody.wu2@landisgyr.com>
2023-08-21 22:35:49 +0800

    write to raw file

--- wf-recv.mjs
+++ wf-recv.mjs
@@ -25,7 +25,7 @@ const MESSAGE_SEQNO_LEN = 4;
 const MESSAGE_TIMESTAMP_LEN = 8;
 const MESSAGE_LEN_SZ = 4;
 const MESSAGE_HEAD_LEN = MESSAGE_START_LEN + MESSAGE_SEQNO_LEN + MESSAGE_TIMESTAMP_LEN + MESSAGE_LEN_SZ;
-const MAX_ROWS_EACH_FILE = 3.84e6;
+const MAX_ROWS_EACH_FILE = 1.92e6;
 const FILE_HANDLER = 'wf-overview.R';
 
 function putData(workpad, data)
@@ -33,7 +33,7 @@ function putData(workpad, data)
     workpad.instream = [...workpad.instream, ...data];
 }
 
-function parseStream(workpad, ws)
+function parseStream(workpad)
 {
     const handleFrame = (frame, frameCounter) => {
         //console.log(frameCounter + ':', dump(frame).slice(10, 10 + 2*15 + 7));
@@ -63,7 +63,7 @@ function parseStream(workpad, ws)
                 pos += 2;
             }
         }
-        writeCsvRow(line, workpad.csvInfo, workpad.execDir);
+        writeCsvRow(line, workpad);
         return true;
     };
 
@@ -143,9 +143,19 @@ const argv = yargs(process.argv.slice(2))
             describe: 'remote tcp port',
             type: 'number',
         },
-        'c': {
-            alias: 'csv',
-            describe: 'csv output filename',
+        'o': {
+            alias: 'out',
+            describe: 'output filename for saving csv or raw stream',
+            type: 'string',
+        },
+        'r': {
+            alias: 'raw',
+            describe: 'saving raw stream instead of csv',
+            type: 'boolean',
+        },
+        'i': {
+            alias: 'input',
+            describe: 'input raw filename',
             type: 'string',
         },
         'n': {
@@ -172,11 +182,13 @@ const workpad = {
             }
         };
     })(10),
+    ws: null,
+    outFilename: argv.out,
     csvInfo: null,
 }
 
-if (argv.csv) {
-    const { dir, name, ext } = path.parse(argv.csv);
+if (argv.out && ! argv.raw) {
+    const { dir, name, ext } = path.parse(argv.out);
     workpad.csvInfo = {
         dir,
         name,
@@ -184,74 +196,114 @@ if (argv.csv) {
         fileCounter: 0,
         rowsCounter: 0,
         header: 'Time,RecvTime,U1,I1,U2,I2,U3,I3',
-        ws: null,
     };
 }
 
-function writeCsvRow(row, csvInfo, execDir)
+function writeCsvRow(row, workpad)
 {
     const makeNextFilename = () => {
-        return path.join(csvInfo.dir,
+        return path.join(workpad.csvInfo.dir,
             [
-                csvInfo.name, '-',
-                (++csvInfo.fileCounter).toString().padStart(5, '0'),
-                csvInfo.ext,
+                workpad.csvInfo.name, '-',
+                (++workpad.csvInfo.fileCounter).toString().padStart(5, '0'),
+                workpad.csvInfo.ext,
             ].join(''));
     };
     const newFile = () => {
         const filename = makeNextFilename();
-        csvInfo.ws = fs.createWriteStream(filename);
-        csvInfo.ws.write(csvInfo.header + '\n');
-        csvInfo.rowCounter = 0;
+        workpad.ws = fs.createWriteStream(filename);
+        workpad.ws.write(workpad.csvInfo.header + '\n');
+        workpad.csvInfo.rowCounter = 0;
         return filename;
     };
     const dispatchFile = filename => {
-        const handlerFilename = path.join(execDir, FILE_HANDLER);
+        const handlerFilename = path.join(workpad.execDir, FILE_HANDLER);
         console.log(`send ${filename} to ${handlerFilename}`);
         exec(`${handlerFilename} -f ${filename}`, (error, stdout, stderr) => {
             if (error)
-                console.error(`dispatch ${filename} error: ${error}`);
+                throw new Error(`dispatch ${filename} error: ${error}`);
             if (stderr)
-                console.error(`exec ${filename} error: ${stderr}`);
+                throw new Error(`exec ${filename} error: ${stderr}`);
         });
     };
 
-    if (! csvInfo) return;
-    if (! csvInfo.ws)
-        csvInfo.filename = newFile();
-    if (csvInfo.rowCounter == MAX_ROWS_EACH_FILE) {
-        csvInfo.ws.end();
-        csvInfo.filename = newFile();
+    if (! workpad.csvInfo) return;
+    if (! workpad.ws)
+        workpad.csvInfo.filename = newFile();
+    if (workpad.csvInfo.rowCounter == MAX_ROWS_EACH_FILE) {
+        workpad.ws.end();
+        dispatchFile(workpad.csvInfo.filename);
+        workpad.csvInfo.filename = newFile();
     }
-    csvInfo.ws.write(row + '\n');
-    ++csvInfo.rowCounter;
+    workpad.ws.write(row + '\n');
+    ++workpad.csvInfo.rowCounter;
 }
 
-function endCsv(csvInfo)
+function writeRaw(data, workpad)
 {
-    if (csvInfo && csvInfo.ws)
-        ws.end();
+    if (! workpad.ws)
+        workpad.ws = fs.createWriteStream(workpad.outFilename);
+    workpad.ws.write(data);
 }
 
-const client = new net.Socket();
-var recvCnt = 0;
+function endOutStream(workpad)
+{
+    if (workpad.ws) workpad.ws.end();
+}
 
-client.connect(argv.port, argv.host, () => {
-    console.log('connected');
-});
-client.on('close', () => {
-    endCsv(workpad.csvInfo);
-    console.log('connection closed');
-});
-client.on('data', data => {
+/**
+ * Handle input data.
+ * @data input data
+ * @return true if end of handling, otherwise false.
+ */
+function handleInData(data)
+{
     try {
+        if (argv.raw) {
+            writeRaw(data, workpad);
+            return false;
+        }
         putData(workpad, data);
         parseStream(workpad);
         if (argv.frames && workpad.frameCounter >= argv.frames)
-            client.end();
+            return true;
     } catch (e) {
-        endCsv(workpad.csvInfo);
+        endOutStream(workpad);
         client.end();
         throw e;
     }
-});
+    return false;
+}
+
+function useSocketClient()
+{
+    const client = new net.Socket();
+
+    client.connect(argv.port, argv.host, () => {
+        console.log('connected');
+    });
+    client.on('close', () => {
+        endOutStream(workpad);
+        console.log('connection closed');
+    });
+    client.on('data', data => {
+        if (handleInData(data)) client.end();
+    });
+    return client;
+}
+
+if (argv.host) return useSocketClient();
+
+if (argv.input) {
+    const rs = fs.createReadStream(argv.input)
+    rs.on('close', () => {
+        endOutStream(workpad);
+    }
+    rs.on('error', err => {
+        endOutStream(workpad);
+        throw(err);
+    }
+    rs.on('data', data => {
+        if (handleInData(data)) rs.destroy();
+    }
+}
