b18516c97f22ed447fcaa0dbb5fd0b8e23290695
Woody Wu <woody.wu2@landisgyr.com>
2023-08-25 19:29:39 +0800

    wf-recv: fix performance issue

--- wf-recv.mjs
+++ wf-recv.mjs
@@ -28,136 +28,321 @@ const MESSAGE_HEAD_LEN = MESSAGE_START_LEN + MESSAGE_SEQNO_LEN + MESSAGE_TIMESTA
 const MAX_ROWS_EACH_FILE = 5.76e6;
 const FILE_HANDLER = 'wf-overview.R';
 
-function putData(workpad, data)
+const workpad = {
+    execDir: path.dirname(process.argv[1]),
+    ws: null,
+    outFilename: null,
+    dispatch: false,
+};
+
+var csvInfo = null;
+
+const inQueue = [];         // input data queue, in an array of buffers
+
+const messageStream = {
+    buf: [],                // bytes stream which formed by messages.
+    remainingPayload: [],   // not completed frame inside a message
+    nextSeqno: null,        // expected seqno of the next message
+    frameCounter: 0,
+    timestamp: null,
+
+    messagePrinter: (function useMessagePrinter(threshold) {
+        var counter = 0;
+        var len = 0;
+        return (seqno, payload, frameCounter) => {
+            if (++counter == threshold) {
+                console.log(`message seq ${seqno} len ${payload.length}`
+                    + ` data used ${frameCounter * WF_FRAME_LEN}`);
+                counter = 0;
+                len = 0;
+            } else {
+                len += payload.length;
+            }
+        };
+    })(10),
+};
+
+const streamPerf = {
+    lastLen: 0,
+    lastTime: new Date().getTime(),
+    updateCounter: 0,
+};
+
+function putData(workpad, inQueue, data)
 {
-    workpad.inQueue.push(data);
-    if (! workpad.parsing) {
-        workpad.parsing = true;
-        setImmediate(() => {
-            parseStream(workpad);
-        });
-    }
+    inQueue.push(data);
+    if (inQueue.length == 1)
+        parseStream(inQueue, messageStream, workpad);
 }
 
-function parseStream(workpad)
-{
-    const handleFrame = (frame, frameCounter) => {
-        //console.log(frameCounter + ':', dump(frame).slice(10, 10 + 2*15 + 7));
-        var cs = [0, 0];
-        var i = 0;
-        for (const b of frame.slice(WF_FRAME_HEAD_LEN
-            , WF_FRAME_HEAD_LEN + WF_FRAME_PAYLOAD_LEN)) {
-            cs[i % 2] ^= b;
-            ++i;
-        }
-        if (cs[0] != frame[WF_FRAME_HEAD_LEN + WF_FRAME_PAYLOAD_LEN]
-            || cs[1] != frame[WF_FRAME_HEAD_LEN + WF_FRAME_PAYLOAD_LEN + 1]) {
-            console.error('bad frame: ', frameCounter);
-            return false;
-        }
-
-        var line = `${frameCounter},`
-            + `${moment(workpad.timestamp).format('YYYY-MM-DD HH:mm:ss.SSS')}`;
-        var pos = WF_FRAME_HEAD_LEN;
-        var value;
-        for (var channel = 0; channel < 3; ++channel) {
-            for (var q = 0; q < 2; ++q) {
-                value = new DataView(new Uint8Array(
-                    frame.slice(pos, pos + 2)
-                ).buffer).getInt16(0, true);
-                line += ',' + value;
-                pos += 2;
-            }
-        }
-        writeCsvRow(line, workpad);
-        return true;
-    };
-
-    const parsePayload = payload => {
-        const data = [...workpad.remainingPayload, ...payload];
-        var pos = 0;
-
-        /* Here only try to parse whole frames, for last incompleted
-         * frame, leave it for the next message.
+function waitDataDrain() {
+    if (! inQueue.length) {
+        /* it's possible there are still works in the messageStream, but I
+         * cannot simply check the length of the stream because it can contains
+         * incompleted message while the next chunk of data will never come.
          */
-        var last_success = true;
-        var last_successful_pos = 0;
-        while (data.length - pos >= WF_FRAME_LEN) {
-            if (data[pos] == WF_FRAME_SYNC
-                    && handleFrame(data.slice(pos, pos + WF_FRAME_LEN),
-                        workpad.frameCounter)) {
-                pos += WF_FRAME_LEN;
-                last_successful_pos = pos;
-                ++workpad.frameCounter;
-            } else {
-                ++pos;
+        setTimeout(() => {
+            endOutStream(workpad);
+            process.exit();
+        }, 1000);
+        return;
+    }
+    setTimeout(() => {
+        console.log('waiting data drain');
+        waitDataDrain();
+    }, 1000);
+}
+
+function handleFrame (frame, frameCounter, timestamp, workpad)
+{
+    var cs = [0, 0];
+    var i = 0;
+    for (const b of frame.slice(WF_FRAME_HEAD_LEN
+        , WF_FRAME_HEAD_LEN + WF_FRAME_PAYLOAD_LEN)) {
+        cs[i % 2] ^= b;
+        ++i;
+    }
+    if (cs[0] != frame[WF_FRAME_HEAD_LEN + WF_FRAME_PAYLOAD_LEN]
+        || cs[1] != frame[WF_FRAME_HEAD_LEN + WF_FRAME_PAYLOAD_LEN + 1]) {
+        console.error('bad frame: ', frameCounter);
+        return false;
+    }
+
+    var line = `${frameCounter},`
+        + `${moment(timestamp).format('YYYY-MM-DD HH:mm:ss.SSS')}`;
+    var pos = WF_FRAME_HEAD_LEN;
+    var value;
+    for (var channel = 0; channel < 3; ++channel) {
+        for (var q = 0; q < 2; ++q) {
+            value = new DataView(new Uint8Array(
+                frame.slice(pos, pos + 2)
+            ).buffer).getInt16(0, true);
+            line += ',' + value;
+            pos += 2;
+        }
+    }
+    writeCsvRow(line, workpad);
+    return true;
+}
+
+function parsePayload(payload, messageStream, workpad)
+{
+    const data = [...messageStream.remainingPayload, ...payload];
+
+    /* Only try to parse whole frames, for the last incompleted frame,
+     * leave it for the next message.
+     */
+    var last_successful_pos = 0;
+    var pos = 0;
+    while (data.length - pos >= WF_FRAME_LEN) {
+        if (data[pos] == WF_FRAME_SYNC
+                && handleFrame(data.slice(pos, pos + WF_FRAME_LEN),
+                    pos == last_successful_pos
+                        ? messageStream.frameCounter
+                        : messageStream.frameCounter + 1,
+                    messageStream.timestamp, workpad)) {
+            if (pos > last_successful_pos) {
+                console.log(`skipped ${pos - last_successful_pos} bytes`);
                 /* all the continuous not-consumed bytes are counted as a
                  * single bad frame, which should occupy a counter.
                  */
-                if (last_success) {
-                    ++workpad.frameCounter;
-                    last_success = false;
-                }
+                ++messageStream.frameCounter;
             }
+            pos += WF_FRAME_LEN;
+            last_successful_pos = pos;
+            ++messageStream.frameCounter;
+        } else {
+            ++pos;
         }
-        workpad.remainingPayload = last_successful_pos
-            ? data.slice(last_successful_pos) : [];
-    };
+    }
+    messageStream.remainingPayload = data.slice(last_successful_pos);
+}
 
-    const onEnd = () => {
-        workpad.parsing = false;
-        if (workpad.inQueue.length)
+function parseStream(inQueue, messageStream, workpad)
+{
+    /**
+     * @payload payload of a single message which contains N frames, the last
+     *   one may be incompleted.
+     */
+    const tryNext = () => {
+        if (inQueue.length)
             setImmediate(() => {
-                parseStream(workpad);
+                parseStream(inQueue, messageStream, workpad);
             });
     };
 
-    const b = workpad.inQueue.shift();
-    if (! b) { /* TODO: check why this could happen */
-        onEnd();
-        return;
-    }
-    workpad.messageStream.push(...b);
-
-    /* parse and strip a whole message, otherwise keep the stream
-     * data untouched.
+    /* Array has a size limit equals to 2^32 - 2, here we keep the water mark
+     * as half height as the limit.
      */
-
-    if (workpad.messageStream.length < MESSAGE_HEAD_LEN) return;
-    if (workpad.messageStream[0] != MESSAGE_START) {
-        console.error(dump(workpad.messageStream).slice(0, 64));
-        throw new Error('bad message');
-    }
-    const messageSeqno = new DataView(
-        new Uint8Array(workpad.messageStream.slice(
-            MESSAGE_START_LEN, MESSAGE_START_LEN + MESSAGE_SEQNO_LEN)).buffer)
-        .getUint32(0)
-    const payloadLen = new DataView(
-        new Uint8Array(workpad.messageStream.slice(
-            MESSAGE_START_LEN + MESSAGE_SEQNO_LEN + MESSAGE_TIMESTAMP_LEN
-            , MESSAGE_START_LEN + MESSAGE_SEQNO_LEN + MESSAGE_TIMESTAMP_LEN + MESSAGE_LEN_SZ)).buffer)
-        .getUint32(0)
-    if (workpad.messageStream.length < MESSAGE_HEAD_LEN + payloadLen) {
-        onEnd();
+    if (! inQueue[0] || inQueue[0].length + messageStream.buf.length > 2**32/2) {
+        tryNext();
         return;
     }
 
-    if (workpad.messageSeqno === null) workpad.messageSeqno = messageSeqno;
-    if (messageSeqno != workpad.messageSeqno)
-        throw new Error(`incorrect message seqno. received ${messageSeqno}` +
-            ` expected ${workpad.messageSeqno}`)
-    workpad.timestamp = new Date(Number(new DataView(
-        new Uint8Array(workpad.messageStream.slice(MESSAGE_START_LEN + MESSAGE_SEQNO_LEN
-            , MESSAGE_START_LEN + MESSAGE_SEQNO_LEN + MESSAGE_TIMESTAMP_LEN)).buffer)
-        .getBigInt64()))
-    const payload = workpad.messageStream.slice(MESSAGE_HEAD_LEN, MESSAGE_HEAD_LEN + payloadLen);
-    workpad.messagePrinter(messageSeqno, payload, workpad.frameCounter);
-    workpad.messageStream = workpad.messageStream.slice(MESSAGE_HEAD_LEN + payloadLen);
-    ++workpad.messageSeqno;
-    parsePayload(payload);
+    const b = inQueue.shift();
 
-    onEnd();
+    updateStreamPerf(streamPerf, messageStream.buf.length, b.length);
+    if (! (streamPerf.updateCounter % 10))
+        console.log(`new chunk len: ${b.length} `
+            + `buf: ${streamPerf.lastLen} queue: ${inQueue.length} `
+            + ` consuming speed: ${streamPerf.kbps.toFixed(3)} kpbs`);
+    messageStream.buf.push(...b);
+
+    /* Parse and strip whole messages, incompleted message will be left in
+     * the stream buf intouched.
+     */
+    while (messageStream.buf.length >= MESSAGE_HEAD_LEN) {
+        if (messageStream.buf[0] != MESSAGE_START) {
+            console.error(dump(messageStream.buf).slice(0, 64));
+            throw new Error('bad message');
+        }
+        const messageSeqno = new DataView(
+            new Uint8Array(messageStream.buf.slice(
+                MESSAGE_START_LEN, MESSAGE_START_LEN + MESSAGE_SEQNO_LEN)).buffer)
+            .getUint32(0)
+        const payloadLen = new DataView(
+            new Uint8Array(messageStream.buf.slice(
+                MESSAGE_START_LEN + MESSAGE_SEQNO_LEN + MESSAGE_TIMESTAMP_LEN
+                , MESSAGE_START_LEN + MESSAGE_SEQNO_LEN
+                    + MESSAGE_TIMESTAMP_LEN + MESSAGE_LEN_SZ)).buffer)
+            .getUint32(0)
+        if (messageStream.buf.length < MESSAGE_HEAD_LEN + payloadLen)
+            break;
+
+        if (messageStream.nextSeqno === null)
+            messageStream.nextSeqno = messageSeqno;
+        if (messageSeqno != messageStream.nextSeqno)
+            throw new Error(`incorrect message seqno. received ${messageSeqno}` +
+                ` expected ${messageStream.nextSeqno}`)
+        messageStream.timestamp = new Date(Number(new DataView(
+            new Uint8Array(messageStream.buf.slice(
+                MESSAGE_START_LEN + MESSAGE_SEQNO_LEN,
+                MESSAGE_START_LEN + MESSAGE_SEQNO_LEN + MESSAGE_TIMESTAMP_LEN)).buffer)
+            .getBigInt64()))
+        const payload = messageStream.buf.slice(
+            MESSAGE_HEAD_LEN, MESSAGE_HEAD_LEN + payloadLen);
+        messageStream.messagePrinter(messageSeqno, payload,
+            messageStream.frameCounter);
+        messageStream.buf = messageStream.buf.slice(
+            MESSAGE_HEAD_LEN + payloadLen);
+        ++messageStream.nextSeqno;
+        parsePayload(payload, messageStream, workpad);
+    }
+
+    tryNext();
 }
+
+function updateStreamPerf(perf, currLen, appendLen)
+{
+    const consumed = perf.lastLen - currLen;
+    perf.kbps = consumed * 8 / (new Date().getTime() - perf.lastTime);
+
+    perf.lastLen = currLen + appendLen;
+    perf.lastTime = new Date().getTime();
+    ++perf.updateCounter;
+}
+
+function writeCsvRow(row, workpad)
+{
+    const makeNextFilename = () => {
+        return path.join(csvInfo.dir,
+            [
+                csvInfo.name, '-',
+                (++csvInfo.fileCounter).toString().padStart(5, '0'),
+                csvInfo.ext,
+            ].join(''));
+    };
+    const newFile = () => {
+        const filename = makeNextFilename();
+        workpad.ws = fs.createWriteStream(filename);
+        workpad.ws.write(csvInfo.header + '\n');
+        csvInfo.rowCounter = 0;
+        return filename;
+    };
+    const dispatchFile = filename => {
+        const handlerFilename = path.join(workpad.execDir, FILE_HANDLER);
+        console.log(`send ${filename} to ${handlerFilename}`);
+        const cmdline = `${handlerFilename} -f ${filename}`;
+        exec(cmdline, (error, stdout, stderr) => {
+            if (error)
+                throw new Error(`exec ${cmdline} error: ${error}`);
+        });
+    };
+
+    if (! csvInfo) return;
+    if (! workpad.ws)
+        csvInfo.filename = newFile();
+    if (csvInfo.rowCounter == MAX_ROWS_EACH_FILE) {
+        endOutStream(workpad);
+        if (workpad.dispatch) dispatchFile(csvInfo.filename);
+        csvInfo.filename = newFile();
+    }
+    workpad.ws.write(row + '\n');
+    ++csvInfo.rowCounter;
+}
+
+function writeRaw(data, workpad)
+{
+    if (! workpad.ws) {
+        workpad.ws = workpad.outfFilename == '-' ? process.stdout
+            : fs.createWriteStream(workpad.outFilename);
+    }
+    workpad.ws.write(data);
+}
+
+function endOutStream(workpad)
+{
+    if (workpad.ws) workpad.ws.end();
+}
+
+/**
+ * Handle input data.
+ * @data input data
+ * @return true if end of handling, otherwise false.
+ */
+function handleInData(data)
+{
+    try {
+        if (argv.raw) {
+            writeRaw(data, workpad);
+            return false;
+        }
+        putData(workpad, inQueue, data);
+        if (argv.frames && messageStream.frameCounter >= argv.frames)
+            return true;
+    } catch (e) {
+        endOutStream(workpad);
+        throw e;
+    }
+    return false;
+}
+
+function useSocketClient()
+{
+    const client = new net.Socket();
+
+    client.connect(argv.port, argv.host, () => {
+        console.error('connected');
+    });
+    client.on('error', e => {
+        throw(e); 
+    });
+    client.on('close', () => {
+        console.error('connection closed');
+        waitDataDrain();
+    });
+    client.on('data', data => {
+        try {
+            if (handleInData(data)) client.end();
+        } catch (e) {
+            client.end();
+            throw(e);
+        }
+    });
+    return client;
+}
+
+/*---------------------------------------------------------------------------*/
 
 const argv = yargs(process.argv.slice(2))
     .option({
@@ -198,42 +383,12 @@ const argv = yargs(process.argv.slice(2))
         },
     }).argv;
 
-const workpad = {
-    execDir: path.dirname(process.argv[1]),
-    inQueue: [],            // input data queue, in an array of buffers
-    messageStream: [],      // parsing from here messages
-    remainingPayload: [],   // not completed frame inside a message
-    parsing: false,         // message parsing is running
-    timestamp: null,
-    messageSeqno: null,
-    frameCounter: 0,
-    messagePrinter: (function useMessagePrinter(threshold) {
-        var counter = 0;
-        var lastTime = new Date().getTime();
-        var len = 0;
-        return (seqno, payload, frameCounter) => {
-            if (++counter == threshold) {
-                console.log(`message seq ${seqno} len ${payload.length}`
-                    + ` data len ${frameCounter * WF_FRAME_LEN}`
-                    + ` ${( len * 8/(new Date().getTime() - lastTime) ).toFixed(3)}`
-                    + ' kbps');
-                counter = 0;
-                lastTime = new Date().getTime();
-                len = 0;
-            } else {
-                len += payload.length;
-            }
-        };
-    })(10),
-    ws: null,
-    outFilename: argv.out,
-    csvInfo: null,
-    dispatch: argv.dispatch,
-}
+workpad.outFilename = argv.out;
+workpad.dispatch = argv.dispatch;
 
 if (argv.out && ! argv.raw) {
     const { dir, name, ext } = path.parse(argv.out);
-    workpad.csvInfo = {
+    csvInfo = {
         dir,
         name,
         ext,
@@ -243,113 +398,13 @@ if (argv.out && ! argv.raw) {
     };
 }
 
-function writeCsvRow(row, workpad)
-{
-    const makeNextFilename = () => {
-        return path.join(workpad.csvInfo.dir,
-            [
-                workpad.csvInfo.name, '-',
-                (++workpad.csvInfo.fileCounter).toString().padStart(5, '0'),
-                workpad.csvInfo.ext,
-            ].join(''));
-    };
-    const newFile = () => {
-        const filename = makeNextFilename();
-        workpad.ws = fs.createWriteStream(filename);
-        workpad.ws.write(workpad.csvInfo.header + '\n');
-        workpad.csvInfo.rowCounter = 0;
-        return filename;
-    };
-    const dispatchFile = filename => {
-        const handlerFilename = path.join(workpad.execDir, FILE_HANDLER);
-        console.log(`send ${filename} to ${handlerFilename}`);
-        const cmdline = `${handlerFilename} -f ${filename}`;
-        exec(cmdline, (error, stdout, stderr) => {
-            if (error)
-                throw new Error(`exec ${cmdline} error: ${error}`);
-        });
-    };
-
-    if (! workpad.csvInfo) return;
-    if (! workpad.ws)
-        workpad.csvInfo.filename = newFile();
-    if (workpad.csvInfo.rowCounter == MAX_ROWS_EACH_FILE) {
-        workpad.ws.end();
-        if (workpad.dispatch) dispatchFile(workpad.csvInfo.filename);
-        workpad.csvInfo.filename = newFile();
-    }
-    workpad.ws.write(row + '\n');
-    ++workpad.csvInfo.rowCounter;
-}
-
-function writeRaw(data, workpad)
-{
-    if (! workpad.ws) {
-        workpad.ws = workpad.outfFilename == '-' ? process.stdout
-            : fs.createWriteStream(workpad.outFilename);
-    }
-    workpad.ws.write(data);
-}
-
-function endOutStream(workpad)
-{
-    if (workpad.ws) workpad.ws.end();
-}
-
-/**
- * Handle input data.
- * @data input data
- * @return true if end of handling, otherwise false.
- */
-function handleInData(data)
-{
-    try {
-        if (argv.raw) {
-            writeRaw(data, workpad);
-            return false;
-        }
-        putData(workpad, data);
-        if (argv.frames && workpad.frameCounter >= argv.frames)
-            return true;
-    } catch (e) {
-        endOutStream(workpad);
-        throw e;
-    }
-    return false;
-}
-
-function useSocketClient()
-{
-    const client = new net.Socket();
-
-    client.connect(argv.port, argv.host, () => {
-        console.error('connected');
-    });
-    client.on('error', e => {
-        throw(e); 
-    });
-    client.on('close', () => {
-        endOutStream(workpad);
-        console.error('connection closed');
-    });
-    client.on('data', data => {
-        try {
-            if (handleInData(data)) client.end();
-        } catch (e) {
-            client.end();
-            throw(e);
-        }
-    });
-    return client;
-}
-
 if (argv.host)
     useSocketClient();
 else if (argv.input) {
     const rs = argv.input == '-' ? process.stdin
         : fs.createReadStream(argv.input)
     rs.on('close', () => {
-        endOutStream(workpad);
+        waitDataDrain();
     });
     rs.on('error', err => {
         endOutStream(workpad);
