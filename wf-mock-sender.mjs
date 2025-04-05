0422f95635deec6d7cc8431c2271b1f961cc28e7
Woody <woody.wu2@honeywell.com>
2025-04-05 20:16:18 +0800

    write to csv

--- wf-mock-sender.mjs
+++ wf-mock-sender.mjs
@@ -8,6 +8,7 @@
 
 import { SerialPort } from 'serialport';
 import yargs from 'yargs/yargs';
+import fs from 'node:fs';
 
 const S_FREQ = 6.4e3;
 
@@ -16,32 +17,37 @@ var sampleGenerator;
 /**
  * singalFreq: AC singal frequency
  * sampleFreq: Sampling l frequency
- * vPeak: Peak voltage
- * aPeak: Peak current
- * vScaler: voltage scaler
- * aScaler: current scaler
+ * uPeak: Peak voltages
+ * iPeak: Peak currents
+ * uk: voltage scaler
+ * ik: current scaler
  * phaseShifts: phase shift of each of three lines in radians
+ * csv: write stream for saving the sample data 
  */
 function useSampleGenerator(format, signalFreq, sampleFreq
-    , vPeak, aPeak, vScaler, aScaler, phaseShifts)
+    , uPeak, iPeak, uk, ik, phaseShifts)
 {
-    return n => {
+    return (n, csv) => {
         const payloadLen = 12;
         const frame = [0xa5];
         var cs0 = 0, cs1 = 0;
+        const sample = [];
         for (var line = 0; line < 3; ++line) {
-            const v = Math.round(vPeak * Math.cos(
-                2*Math.PI * signalFreq/sampleFreq * n + line * 2 * Math.PI / 3
-            ) * vScaler);
-            const a = Math.round(aPeak * Math.cos(
-                2*Math.PI * signalFreq/sampleFreq * n + line * 2 * Math.PI / 3
-                - phaseShifts[line]
-            ) * aScaler);
+            const freqComp = 2*Math.PI * signalFreq/sampleFreq * n;
+            const lineDelay = line * 2 * Math.PI / 3;
+            const u = uPeak[line] * Math.cos(
+                freqComp + lineDelay
+            );
+            const i = iPeak[line] * Math.cos(
+                freqComp + lineDelay - phaseShifts[line]
+            );
+            const uScaled = Math.round(u * uk);
+            const iScaled = Math.round(i * ik);
 
             var b0, b1;
             const view = new DataView(new ArrayBuffer(2));
 
-            view.setInt16(0, v, true);
+            view.setInt16(0, uScaled, true);
             b0 = view.getUint8(0);
             b1 = view.getUint8(1);
             frame.push(b0);
@@ -49,14 +55,37 @@ function useSampleGenerator(format, signalFreq, sampleFreq
             cs0 ^= b0;
             cs1 ^= b1;
 
-            view.setInt16(0, a, true);
+            view.setInt16(0, iScaled, true);
             b0 = view.getUint8(0);
             b1 = view.getUint8(1);
             frame.push(b0);
             frame.push(b1);
             cs0 ^= b0;
             cs1 ^= b1;
+
+            sample.push({
+                u,
+                uScaled,
+                i,
+                iScaled,
+            });
         }
+        if (csv)
+            csv.write(`${n},`
+                + `${sample[0].u.toFixed(3)},`
+                + `${sample[0].i.toFixed(3)},`
+                + `${sample[1].u.toFixed(3)},`
+                + `${sample[1].i.toFixed(3)},`
+                + `${sample[2].u.toFixed(3)},`
+                + `${sample[2].i.toFixed(3)},`
+                + `${sample[0].uScaled},`
+                + `${sample[0].iScaled},`
+                + `${sample[1].uScaled},`
+                + `${sample[1].iScaled},`
+                + `${sample[2].uScaled},`
+                + `${sample[2].iScaled}`
+                + '\n'
+            );
         if (format == 'au') {
             frame.push(cs0);
             frame.push(cs1);
@@ -65,15 +94,15 @@ function useSampleGenerator(format, signalFreq, sampleFreq
     };
 }
 
-function generateSamples(nStart, nEnd)
+function generateSamples(nStart, nEnd, csv)
 {
     var bytes = [];
     for (var i = nStart; i < nEnd; ++i)
-        bytes = [...bytes, ...sampleGenerator(i)];
+        bytes = [...bytes, ...sampleGenerator(i, csv)];
     return bytes;
 }
 
-function wrLoop(stream, max_samples, BER)
+function wrLoop(serial, max_samples, BER, csv)
 {
     const period = 25;
     const speedGain = 1.65;
@@ -88,7 +117,7 @@ function wrLoop(stream, max_samples, BER)
         var end = n + step;
         if (max_samples > 0 && end > max_samples)
             end = max_samples;
-        const data = generateSamples(n, end);
+        const data = generateSamples(n, end, csv);
         octetsSent = data.length;
         if (BER && octetsSent >= BER) {
             const idx1 = parseInt(Math.random() * data.length);
@@ -96,10 +125,13 @@ function wrLoop(stream, max_samples, BER)
             data[idx1] ^= 1 << idx2;
             octetsSent = 0;
         }
-        stream.write(data);
+        serial.write(data);
 
-        if (end == max_samples)
-            process.exit(0);
+        if (end == max_samples) {
+            csv.end();
+            serial.end();
+            return;
+        }
 
         ++stat_cnt;
         if (stat_cnt == 100) {
@@ -151,6 +183,35 @@ const argv = yargs(process.argv.slice(2))
             type: 'number',
             default: 0,
         },
+        'P': {
+            alias: 'phi',
+            describe: 'Phase shift of each line in comma separated degrees, e.g., 0,-15,20',
+            type: 'string',
+            default: '0,0,0',
+        },
+        'S': {
+            alias: 'scaler',
+            describe: 'Inverse scalers for voltage and current, separated by comma',
+            type: 'string',
+            default: '2.1522e-2,4.61806e-3',
+        },
+        'V': {
+            alias: 'uPeak',
+            describe: 'Peak voltages of each line in comma separated volts',
+            type: 'number',
+            default: '311,311,311',
+        },
+        'A': {
+            alias: 'iPeak',
+            describe: 'Peak currents of each line in comma separated amps',
+            type: 'number',
+            default: '15,15,15',
+        },
+        'c': {
+            alias: 'csv',
+            describe: 'Csv file to save the generated sample data',
+            type: 'string',
+        },
     }).argv;
 
 if (argv.number < 0) {
@@ -161,11 +222,43 @@ if (argv.BER < 0) {
     console.error("BER must be a non-negative integer.");
     process.exit(1);
 }
+var phi = argv.phi.split(',');
+if (phi.length != 3) {
+    console.error('phase shifts need to be three degrees');
+    process.exit(1);
+}
+phi = phi.map(x => parseFloat(x)/180 * Math.PI);
 
+var scaler = argv.scaler.split(',');
+if (scaler.length != 2) {
+    console.error('Need two scalers');
+    process.exit(1);
+}
+scaler = scaler.map(x => 1/parseFloat(x));
+
+var uPeak = argv.uPeak.split(',');
+var iPeak = argv.iPeak.split(',');
+if (uPeak.length != 3) {
+    console.error('There are three V\'s needed');
+    process.exit(1);
+}
+uPeak = uPeak.map(x => parseFloat(x));
+if (iPeak.length != 3) {
+    console.error('There are three A\'s needed');
+    process.exit(1);
+}
+iPeak = iPeak.map(x => parseFloat(x));
+
+var ws = null;
+if (argv.csv) {
+    ws = fs.createWriteStream(argv.csv);
+    ws.write('Seqno,u1,i1,u2,i2,u3,i3,'
+        + 'u1Scaled,i1Scaled,u2Scaled,i2,i2Scaled,u3Scaled,i3Scaled\n');
+}
 sampleGenerator = useSampleGenerator(argv.format, 50, S_FREQ
-    , 220*Math.sqrt(2), 10*Math.sqrt(2)
-    , 1/2.1522e-2, 1/4.61806e-3
-    , [0, (30/180)*Math.PI, (-30/180)*Math.PI]);
+    , uPeak, iPeak
+    , scaler[0], scaler[1]
+    , phi);
 
 const seri = new SerialPort({
     path: argv.device,
@@ -175,7 +268,10 @@ const seri = new SerialPort({
 
 seri.open(err => {
     if (err) throw new Error(err);
-    wrLoop(seri, argv.number, argv.BER);
+    wrLoop(seri, argv.number, argv.BER, ws);
+    ws.on('close', () => {
+        process.exit(0);
+    });
 });
 seri.on('data', () => {
 });
